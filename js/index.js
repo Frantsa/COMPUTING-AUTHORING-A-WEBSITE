615bc3fcfd8257c4e047ae69a3da9557dcc54bba
vladislava.frantsishkevych <vladkafranse@gmail.com>
2023-12-22 01:56:39 +0000

    add more

--- js/index.js
+++ js/index.js
@@ -4,43 +4,43 @@
             id: 1,
             image: 'img/index/things-to-do/horseriding.jpg',
             title: 'Horse Riding',
-            price: '$4',
+            price: '£4',
             description: 'Take a measured romantic horseback riding tour and enjoy our unforgettable picturesque surroundings.'
         },
         {
             id: 2,
             image: 'img/index/things-to-do/rockclimbing.jpg',
             title: 'Rock Climbing',
-            price: '$2',
-            description: 'Whether you are a professional mountaineer or it’s your first time, Yosemite one of the best places for this kind of sport.'
+            price: '£2',
+            description: 'Whether you are a professional mountaineer or it’s your first time, Tinto one of the best places for this kind of sport.'
         },
         {
             id: 3,
             image: 'img/index/things-to-do/biking.jpg',
             title: 'Biking',
-            price: '$1',
-            description: 'Rent a bike and take a fun and easy two-wheel tour over Yosemite Valley. It usually takes 2 hours.'
+            price: '£1',
+            description: 'Rent a bike and go on a fun and easy two-wheeled journey through the Scottish countryside.'
         },
         {
             id: 4,
             image: 'img/index/things-to-do/rafting.jpg',
             title: 'Rafting',
-            price: '$3',
+            price: '£3',
             description: 'You have a perfect choice to combine your river rafting adventure with a visit to the park. Test our waters.'
         },
         {
             id: 5,
             image: 'img/index/things-to-do/fishing.jpg',
             title: 'Fishing',
-            price: '$2',
+            price: '£2',
             description: 'With a number of permanent streams and mountain lakes, we offer anglers of all levels a diverse variety of opportunities for fishing.'
         },
         {
             id: 6,
-            image: 'img/index/things-to-do/spas.jpg',
-            title: 'Spas and Wellness',
-            price: '$2',
-            description: 'After a long exhausting day, you can relax and pamper yourself in our unique spa and wellness center.'
+            image: 'img/index/things-to-do/zoo.jpg',
+            title: 'Family Zoo',
+            price: '£2',
+            description: 'A petting zoo is a great way to spend time with our smaller friends.'
         }
     ];
 
